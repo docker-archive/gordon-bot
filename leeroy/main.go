3164ac58486ade2a3758de2b1faced0211ec36f2
Jessica Frazelle <jess@docker.com>
2015-02-11 22:44:44 -0800

    update types

--- leeroy/main.go
+++ leeroy/main.go
@@ -6,17 +6,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/fabioxgn/go-bot"
 )
 
 var (
 	repo string = "docker/docker"
-	url  string = "https://leeroy.dockerproject.com/retry"
+	url  string = "https://leeroy.dockerproject.com/build/retry"
 )
 
 type PullRequest struct {
-	Number string `json:"number"`
+	Number int    `json:"number"`
 	Repo   string `json:"repo"`
 }
 
@@ -28,8 +29,14 @@ func rebuild(command *bot.Cmd) (msg string, err error) {
 		repo = command.Args[1]
 	}
 
+	// convert the string to an int
+	num, err := strconv.Atoi(command.Args[0])
+	if err != nil {
+		return "", fmt.Errorf("converting PR num to int failed: %v", err)
+	}
+
 	pr := PullRequest{
-		Number: command.Args[0],
+		Number: num,
 		Repo:   repo,
 	}
 	data, err := json.Marshal(pr)
@@ -52,10 +59,10 @@ func rebuild(command *bot.Cmd) (msg string, err error) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 204 {
-		return fmt.Sprintf("Rebuilding PR %s for %s returned status code: %d", pr.Number, repo, resp.StatusCode), nil
+		return fmt.Sprintf("Rebuilding PR %d for %s returned status code: %d", pr.Number, repo, resp.StatusCode), nil
 	}
 
-	return fmt.Sprintf("Rebuilding PR %s at https://github.com/%s/pull/%s", pr.Number, repo, pr.Number), nil
+	return fmt.Sprintf("Rebuilding PR %d at https://github.com/%s/pull/%d", pr.Number, repo, pr.Number), nil
 }
 
 func init() {
