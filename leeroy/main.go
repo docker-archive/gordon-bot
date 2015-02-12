928b2ed25e8c0c4a51cec177d4a80d288cac0a25
Jessica Frazelle <jess@docker.com>
2015-02-12 01:33:36 -0800

    custom builds

--- leeroy/main.go
+++ leeroy/main.go
@@ -12,13 +12,41 @@ import (
 )
 
 var (
-	repo string = "docker/docker"
-	url  string = "https://leeroy.dockerproject.com/build/retry"
+	repo    string = "docker/docker"
+	baseurl string = "https://leeroy.dockerproject.com/"
 )
 
 type PullRequest struct {
-	Number int    `json:"number"`
-	Repo   string `json:"repo"`
+	Number  int    `json:"number"`
+	Repo    string `json:"repo"`
+	Context string `json:"context"`
+}
+
+func sendRequest(pr PullRequest, url string) (err error) {
+	data, err := json.Marshal(pr)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(os.Getenv("LEEROY_USERNAME"), os.Getenv("LEEROY_PASS"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 204 {
+		return fmt.Errorf("Requesting %s for PR %d for %s returned status code: %d", url, pr.Number, repo, resp.StatusCode)
+	}
+
+	return nil
 }
 
 func rebuild(command *bot.Cmd) (msg string, err error) {
@@ -39,30 +67,47 @@ func rebuild(command *bot.Cmd) (msg string, err error) {
 		Number: num,
 		Repo:   repo,
 	}
-	data, err := json.Marshal(pr)
-	if err != nil {
+
+	if err := sendRequest(pr, baseurl+"build/retry"); err != nil {
 		return "", err
-	}
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	if err != nil {
-		return "", err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(os.Getenv("LEEROY_USERNAME"), os.Getenv("LEEROY_PASS"))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", nil
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 204 {
-		return fmt.Sprintf("Rebuilding PR %d for %s returned status code: %d", pr.Number, repo, resp.StatusCode), nil
 	}
 
 	return fmt.Sprintf("Rebuilding PR %d at https://github.com/%s/pull/%d", pr.Number, repo, pr.Number), nil
+}
+
+func customBuild(command *bot.Cmd, context string) (msg string, err error) {
+	if len(command.Args) < 1 {
+		return fmt.Sprintf("Not enough args. try: !%s 9437 docker/docker", context), nil
+	}
+	if len(command.Args) >= 2 {
+		repo = command.Args[1]
+	}
+
+	// convert the string to an int
+	num, err := strconv.Atoi(command.Args[0])
+	if err != nil {
+		return "", fmt.Errorf("converting PR num to int failed: %v", err)
+	}
+
+	pr := PullRequest{
+		Number:  num,
+		Repo:    repo,
+		Context: context,
+	}
+
+	if err := sendRequest(pr, baseurl+"build/custom"); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("Building PR %d on %s at https://github.com/%s/pull/%d", pr.Number, context, repo, pr.Number), nil
+}
+
+func lxcBuild(command *bot.Cmd) (msg string, err error) {
+	return customBuild(command, "lxc")
+}
+
+func windowsBuild(command *bot.Cmd) (msg string, err error) {
+	return customBuild(command, "windows")
 }
 
 func init() {
@@ -71,4 +116,18 @@ func init() {
 		"Rebuilds a PR number on Jenkins.",
 		"9437",
 		rebuild)
+
+	bot.RegisterCommand(
+		"lxc",
+		"Build a PR on an lxc box with the lxc driver",
+		"9345",
+		lxcBuild,
+	)
+
+	bot.RegisterCommand(
+		"windows",
+		"Build a PR on a windows box",
+		"9345",
+		windowsBuild,
+	)
 }
